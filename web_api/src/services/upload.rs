b79bc799fbd35f4a39ee9be5324e044e41e8a345
FelipeCN <felipecaninn14@gmail.com>
2023-12-28 16:48:32 -0300

    Update file upload path for NFE XML files

--- web_api/src/services/upload.rs
+++ web_api/src/services/upload.rs
@@ -8,19 +8,24 @@ pub async fn upload(mut multipart: Multipart) {
     dotenv().ok();
     while let Some(field) = multipart.next_field().await.unwrap() {
         let name = field.name().unwrap().to_string();
-        if name == "file" {
-            let time = Utc::now()
+        let time = Utc::now()
                 .to_string()
                 .split_whitespace()
                 .collect::<Vec<_>>()
                 .join("")
                 .replace(['.', ':', '-'], "")
                 .replace("UTC", "");
-            let data = field.bytes().await.unwrap();
-            let path = dotenv::var("UPLOAD_PATH").unwrap();
-            let file_name = format!("{}/{}.xml", path, time);
-            let mut file = File::create(file_name).await.unwrap();
-            file.write_all(&data).await.unwrap();
+        let data = field.bytes().await.unwrap();
+        let path = dotenv::var("UPLOAD_PATH").unwrap();
+        match name.as_str() {
+            "nfe" => {
+                let file_name = format!("{}/nfe/{}.xml", path, time);
+                let mut file = File::create(file_name).await.unwrap();
+                file.write_all(&data).await.unwrap();
+            }
+            _ => {
+                // Handle other field names here
+            }
         }
     }
 }
