53f0172a75e6fb9b0b16a3eb76744696afae221e
FelipeCN <felipecaninn14@gmail.com>
2024-01-16 16:48:58 -0300

    Refactor created_at variable type to NaiveDate

--- core_sql/src/modules/usuarios/insert.rs
+++ core_sql/src/modules/usuarios/insert.rs
@@ -8,8 +8,8 @@ use crate::structs::usuarios::struct_user::CreateUserModel;
 #[allow(dead_code)]
 pub async fn insert_user(pool: &PgPool, user: CreateUserModel) -> Result<(), Box<dyn Error>> {
     let uuid = Uuid::new_v4();
-    let created_at = Utc::now().naive_utc().date(); // Change the type to NaiveDate
-    
+    let created_at = Utc::now().naive_utc().date(); 
+
     let result = sqlx::query!(
         r#"INSERT INTO users (iduser, firstname, secondname, email, password, created_at) VALUES ($1, $2, $3, $4, $5, $6)"#,
         uuid,
@@ -21,7 +21,6 @@ pub async fn insert_user(pool: &PgPool, user: CreateUserModel) -> Result<(), Box
     )
     .execute(pool)
     .await?;
-    
 
     match result.rows_affected() {
         1 => Ok(()),
