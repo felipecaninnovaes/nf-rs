3d2d3835a54f453854e7010e5de8cf59350292c4
FelipeCN <felipecaninn14@gmail.com>
2024-01-16 16:45:49 -0300

    Add user insert and update queries

--- core_sql/src/modules/usuarios/insert.rs
+++ core_sql/src/modules/usuarios/insert.rs
@@ -8,13 +8,23 @@ use crate::structs::usuarios::struct_user::CreateUserModel;
 #[allow(dead_code)]
 pub async fn insert_user(pool: &PgPool, user: CreateUserModel) -> Result<(), Box<dyn Error>> {
     let uuid = Uuid::new_v4();
-    let created_at = Utc::now().naive_utc();
-    let insert_user = format!(
-        "INSERT INTO users (iduser, firstname, secondname, email, password, created_at) VALUES ('{}', '{}', '{}', '{}', '{}', '{}')",
-        uuid, user.firstname, user.secondname, user.email, user.password, created_at
-    );
-    match sqlx::query(&insert_user).execute(pool).await {
-        Ok(_) => Ok(()),
-        Err(_) => Err("Erro ao inserir usuario".into()),
+    let created_at = Utc::now().naive_utc().date(); // Change the type to NaiveDate
+    
+    let result = sqlx::query!(
+        r#"INSERT INTO users (iduser, firstname, secondname, email, password, created_at) VALUES ($1, $2, $3, $4, $5, $6)"#,
+        uuid,
+        user.firstname,
+        user.secondname,
+        user.email,
+        user.password,
+        created_at
+    )
+    .execute(pool)
+    .await?;
+    
+
+    match result.rows_affected() {
+        1 => Ok(()),
+        _ => Err("Failed to insert user".into()),
     }
 }
---
+++ core_sql/src/modules/usuarios/update.rs
@@ -0,0 +1,22 @@
+use sqlx::PgPool;
+use std::error::Error;
+
+use crate::structs::usuarios::struct_user::UserModelUpdate;
+
+#[allow(dead_code)]
+pub async fn update(
+    pool: &PgPool,
+    user: UserModelUpdate
+) -> Result<(), Box<dyn Error>> {
+    sqlx::query!(
+        r#"UPDATE users SET firstname = $1, secondname = $2, email = $3 WHERE iduser = $4"#,
+        user.firstname,
+        user.secondname,
+        user.email,
+        user.iduser,
+    )
+    .execute(pool)
+    .await?;
+
+    Ok(())
+}
