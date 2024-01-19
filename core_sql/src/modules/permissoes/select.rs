7cce35be504daf75bf0383e74c8ff1501d552083
FelipeCN <felipecaninn14@gmail.com>
2024-01-18 21:40:25 -0300

    Fix type variables

--- core_sql/src/modules/permissoes/select.rs
+++ core_sql/src/modules/permissoes/select.rs
@@ -9,8 +9,8 @@ pub async fn get_permissions(
     pool: &PgPool,
     user_id: Uuid,
 ) -> Result<Vec<Permissions>, Box<dyn Error>> {
-    let allowed = true;
-    let q_empresas_allowed = "SELECT permissions.permissions_user_id, permissions.permissions_allowed, empresas.cnpj from permissions INNER JOIN empresas ON permissions.permissions_empresa_id = empresas.idempresa where permissions.permissions_user_id = $1 and permissions.permissions_allowed = $2";
+    let allowed: bool = true;
+    let q_empresas_allowed: &str = "SELECT permissions.permissions_user_id, permissions.permissions_allowed, empresas.cnpj from permissions INNER JOIN empresas ON permissions.permissions_empresa_id = empresas.idempresa where permissions.permissions_user_id = $1 and permissions.permissions_allowed = $2";
 
     match sqlx::query_as::<_, Permissions>(q_empresas_allowed)
         .bind(user_id)
