bbdd7abc88f2d775b023f52974d93212ed45b0ff
FelipeCN <felipecaninn14@gmail.com>
2024-01-20 18:05:05 -0300

    Update Error and Sucess from web_api

--- web_api/src/services/users/select.rs
+++ web_api/src/services/users/select.rs
@@ -3,13 +3,20 @@ use sqlx::{Pool, Postgres};
 
 use core_sql::modules::usuarios::select::select_all_users;
 
+use crate::services::utils::{api_error::APIError, api_ok::APIOk};
+
 pub async fn select_users(Extension(_pool): Extension<Pool<Postgres>>) -> impl IntoResponse {
-    let result = select_all_users(&_pool).await.unwrap();
-    match serde_json::to_string(&result) {
-        Ok(json) => (StatusCode::OK, json),
-        Err(_) => (
-            StatusCode::INTERNAL_SERVER_ERROR,
-            "Error to convert json".to_owned(),
-        ),
+    let usuarios = select_all_users(&_pool).await.unwrap();
+    match usuarios.len() {
+        0 => Err(APIError {
+            message: "Nenhum usuarios encontrado".to_owned(),
+            status_code: StatusCode::NOT_FOUND,
+            error_code: Some(44),
+        }),
+        _ => Ok(APIOk {
+            message: "Usuarios encontrados com sucesso".to_owned(),
+            status_code: StatusCode::OK,
+            data: Some(serde_json::to_value(usuarios).unwrap()),
+        }),
     }
 }
